25e13826e9c0fef64cd4123a95b90f26ae786a5c
TakWolf <takwolf@foxmail.com>
2025-03-23 04:26:30 +0800

    升级构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("com.android.library") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("com.android.application") version "8.8.0" apply false
+    id("com.android.library") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
 }
